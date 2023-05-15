6bf07e6738da5422736034c01245bb88f133f423
MrArsars <arseniu437@gmail.com>
2023-05-15 13:38:57 +0300

    added storing products to the cart functionality

--- djangoShop/product/urls.py
+++ djangoShop/product/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from product.views import product
+from product.views import *
 
 urlpatterns = [
-    path('product/<path:productid>/', product),
+    path('product/<int:productid>/', product),
+    path('product/add/', add_to_cart, name = "add"),
+
 ]
--- djangoShop/product/views.py
+++ djangoShop/product/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
+from cart.models import CartItem
 from product.models import Product
 
 
@@ -10,3 +12,11 @@ def product(request, productid):
         'product': db_product,
     }
     return render(request, 'product/ProductPage.html', context)
+
+
+def add_to_cart(request):
+    if request.method == "POST" and request.user.is_authenticated:
+        userId = request.user.id
+        new_item = CartItem(product_id = request.POST['product_id'], user_id = userId)
+        new_item.save()
+    return HttpResponseRedirect(request.get_full_path())
